3e481a4655b7f76095202787af8f2fd8c16884e0
Naiara Saratxaga <99652446+NaiSaratxaga@users.noreply.github.com>
2022-05-08 11:39:44 +0200

    last changes, issues done

--- src/components/App.js
+++ src/components/App.js
@@ -86,61 +86,59 @@ const App = () => {
         <h1 className='header__title'>Frases de Friends</h1>
         <form className='filter'>
           <label className='filter__label' htmlFor='character'>
-            Filtrar por frase
-            <input
-              className='filter'
-              type='text'
-              name='quote'
-              id='quote'
-              value={filterQuote}
-              onChange={handleFilterQuote}
-            />
+            Filtrar por frase:
           </label>
-          <label className='filter__label' htmlFor='character'>
-            Filtrar por personaje
-            <select
-              className='filter__select'
-              value={filterCharacter}
-              onChange={handleFilterCharacter}
-            >
-              <option value='all'>Todos</option>
-              <option value='Ross'>Ross</option>
-              <option value='Monica'>Monica</option>
-              <option value='Joey'>Joey</option>
-              <option value='Phoebe'>Phoebe</option>
-              <option value='Chandler'>Chandler</option>
-              <option value='Rachel'>Rachel</option>
-            </select>
+          <input
+            className='filter__text'
+            type='text'
+            name='quote'
+            id='quote'
+            value={filterQuote}
+            onChange={handleFilterQuote}
+          />
+          <label className='filter__labelCharacter' htmlFor='character'>
+            Filtrar por personaje:
           </label>
+          <select
+            className='filter__select'
+            value={filterCharacter}
+            onChange={handleFilterCharacter}
+          >
+            <option value='all'>Todos</option>
+            <option value='Ross'>Ross</option>
+            <option value='Monica'>Monica</option>
+            <option value='Joey'>Joey</option>
+            <option value='Phoebe'>Phoebe</option>
+            <option value='Chandler'>Chandler</option>
+            <option value='Rachel'>Rachel</option>
+          </select>
         </form>
       </header>
 
       <ul className='quotes__list'>{htmlData}</ul>
 
+      <h2 className='new-quote__title'>Añadir una nueva frase</h2>
       <form className='new-quote__form'>
-        <h2 className='new-quote__title'>Añadir una nueva frase</h2>
-        <label className='new-quote__label' htmlFor='quote'>
-          <span className='new-quote__label-text'>Frase</span>
-          <input
-            className='new-quote__text'
-            type='text'
-            name='quote'
-            id='quote'
-            value={quoteObject.quote}
-            onChange={handleInputs}
-          />
-        </label>
-        <label className='new-quote__label' htmlFor='character'>
-          <span className='new-quote__label-text'>Personaje</span>
-          <input
-            className='new-quote__text'
-            type='text'
-            name='character'
-            id='character'
-            value={quoteObject.character}
-            onChange={handleInputs}
-          />
-        </label>
+        <label className='new-quote__label' htmlFor='quote'></label>
+        <span className='new-quote__label-text'>Frase:</span>
+        <input
+          className='new-quote__text'
+          type='text'
+          name='quote'
+          id='quote'
+          value={quoteObject.quote}
+          onChange={handleInputs}
+        />
+        <label className='new-quote__label' htmlFor='character'></label>
+        <span className='new-quote__label-text'>Personaje:</span>
+        <input
+          className='new-quote__text'
+          type='text'
+          name='character'
+          id='character'
+          value={quoteObject.character}
+          onChange={handleInputs}
+        />
         <button onClick={handleNewQuote} className='new-quote__btn'>
           Añadir un nueva frase
         </button>
